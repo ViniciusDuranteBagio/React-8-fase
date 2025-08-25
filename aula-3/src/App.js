a2cda13ed7e58cb6a80b7c9ee699cb2ac379cc34
ViniMarianoSilva <vini.mariano.silva@hotmail.com>
2025-08-25 16:47:44 -0300

    Atualização do jogo da velha

--- aula-3/src/App.js
+++ aula-3/src/App.js
@@ -1,24 +1,28 @@
+import { useState } from "react";
+
 function Quadrado({ value }) {
+
   return <button className="quadrado">{value}</button>;
 }
 
 export default function Tabuleiro() {
+  const [quadrados, setQuadrados] = useState(Array(9).fill(null));
   return (
     <>
       <div className="linhaTabuleiro">
-        <Quadrado value="1" />
-        <Quadrado value="2" />
-        <Quadrado value="3" />
+        <Quadrado value={quadrados[0]} />
+        <Quadrado value={quadrados[1]} />
+        <Quadrado value={quadrados[2]} />
       </div>
       <div className="linhaTabuleiro">
-        <Quadrado value="4" />
-        <Quadrado value="5" />
-        <Quadrado value="6" />
+        <Quadrado value={quadrados[3]} />
+        <Quadrado value={quadrados[4]} />
+        <Quadrado value={quadrados[5]} />
       </div>
       <div className="linhaTabuleiro">
-        <Quadrado value="7" />
-        <Quadrado value="8" />
-        <Quadrado value="9" />
+        <Quadrado value={quadrados[6]} />
+        <Quadrado value={quadrados[7]} />
+        <Quadrado value={quadrados[8]} />
       </div>
     </>
   );
