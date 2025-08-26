788ce95c52dc7d613db52a77c96a56e3fedf665c
Luiz Fernando Mendes Alberton <141519223+luizfernandomendesalberton@users.noreply.github.com>
2025-08-25 21:23:23 -0300

    atualizando

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Game from '../../3a-aula/src/App.jsx'
-
- 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Game/>
+    <App />
   </StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h2>Bem-vindo ao Jogo da Velha!</h2>
+      <p>Clique em "Play" na navegação para começar a jogar.</p>
+    </div>
+  );
+}
+
+
+export default Home;
