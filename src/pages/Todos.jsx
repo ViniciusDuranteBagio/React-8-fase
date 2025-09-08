d955d2983270c82b3f42d8cd69129f32132811a7
Gabriel0Ramos0 <gabrielrcorrea@outlook.com>
2025-09-08 19:22:02 -0300

    Resolvendo bug 1 e 2

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -14,21 +14,19 @@ export default function Todos() {
   const [todos, setTodos] = useState(loadTodos);
 
   useEffect(() => {
-    localStorage.setItem("todo", JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
   function addTodo() {
     if (!input.trim()) return;
     const newTodo = { id: Date.now(), text: input.trim(), done: false };
     todos.push(newTodo);
-    setTodos(todos);
-    setInput("");
+    setTodos([...todos]);
+    //setInput(""); tirei para facilitar a insersão de testes
   }
 
   function toggleTodo(id) {
-    const t = todos.find(x => x.id === id);
-    if (t) t.done = !t.done;
-    setTodos(todos);
+    setTodos(todos.map(t => t.id === id ? { ...t, done: !t.done } : t));
   }
 
   function removeTodo(id) {
